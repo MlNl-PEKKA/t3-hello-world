fca5d0a21b1d06c8c7062508b1822e102a88d456
PunithG360 <punithg@figmatic.co>
2023-12-18 13:24:28 +0530

    fix: Font setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,19 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
