63934e228046e78c1580218def4d86b25f5ec518
PunithG360 <punithg@figmatic.co>
2023-12-21 22:18:18 +0530

    fix: Zustad hook consumption

--- src/zustandStore/index.tsx
+++ src/zustandStore/index.tsx
@@ -3,21 +3,32 @@ import { combine } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { produce } from "immer";
 
-export const useHomeStore = create(
+export const useFormVal1 = create(
   immer(
-    combine(
-      { formVal1: "", formVal2: "", deepObj: { obj: { a: true } } },
-      (set) => ({
-        setFormVal1: (e: string) => set(() => ({ formVal1: e })),
-        setFormVal2: (e: string) => set(() => ({ formVal2: e })),
-        setFormObj: () =>
-          set(
-            produce((state) => {
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-              state.deepObj.obj.a = !state.deepObj.obj.a;
-            }),
-          ),
-      }),
-    ),
+    combine({ formVal1: "" }, (set) => ({
+      setFormVal1: (e: string) => set(() => ({ formVal1: e })),
+    })),
+  ),
+);
+
+export const useFormVal2 = create(
+  immer(
+    combine({ formVal2: "" }, (set) => ({
+      setFormVal2: (e: string) => set(() => ({ formVal2: e })),
+    })),
+  ),
+);
+
+export const useDeepObj = create(
+  immer(
+    combine({ deepObj: { obj: { a: true } } }, (set) => ({
+      setFormObj: () =>
+        set(
+          produce((state) => {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            state.deepObj.obj.a = !state.deepObj.obj.a;
+          }),
+        ),
+    })),
   ),
 );
